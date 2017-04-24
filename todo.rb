48de2c2273d9014201554face86c2b7f03dec6f0
Daniel Strong <danielstrong@mac.com>
2017-04-24 14:21:19 -0800

    Completed 'add tasks' user story

--- todo.rb
+++ todo.rb
@@ -1,13 +1,35 @@
 # Classes
 class List
-  def intialize
+
+  def initialize
+    @all_tasks = []
   end
+
+  attr_reader :all_tasks
+
+  def add(task)
+    @all_tasks << task
+  end
+
 end
 
-if __FILE__ == $PROGRAM_NAME
-  my_list = List.new
-  puts 'You have created a new list'
+class Task
+
+  def initialize(description)
+    @description = description
+  end
+
+  attr_reader :description
 end
+
 
 
 # actions
+
+# Program runner
+if __FILE__ == $PROGRAM_NAME
+         my_list = List.new
+         puts 'You have created a new list'
+         my_list.add(Task.new('Make Breakfast'))
+         puts 'You have added a task to the Todo List'
+       end
