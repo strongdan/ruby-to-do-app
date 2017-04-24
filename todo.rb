bcda224964548e0f8966738dbb6bdf756e8c01cc
Daniel Strong <danielstrong@mac.com>
2017-04-24 14:44:20 -0800

    add simple menu

--- todo.rb
+++ todo.rb
@@ -1,46 +1,71 @@
 # Classes
-class List
+module Menu
 
-  def initialize
-    @all_tasks = []
-  end
+     def menu
+       " Welcome to the TodoLister Program!
+       This menu will help you use the Task List System
+       1) Add
+       2) Show
+       Q) Quit "
+     end
 
-  attr_reader :all_tasks
-
-  def add(task)
-    @all_tasks << task
-  end
-
-  def show
-    all_tasks
-  end
-
-end
-
-class Task
-  def initialize(description)
-    @description = description
-  end
-
-  attr_reader :description
-end
+     def show
+       menu
+     end
+   end
 
 
+   module Promptable
+     def prompt(message = "Just the facts, ma'am.", symbol = ':> ')
+       print message
+       print symbol
+       gets.chomp
+     end
+   end
 
-# actions
 
-# Program runner
-if __FILE__ == $PROGRAM_NAME
-         my_list = List.new
-         puts 'You have created a new list'
-         my_list.add(Task.new('Make Breakfast'))
-         puts 'You have added a task to the Todo List'
-         if my_list.show.join.include?('#<')
-              print [
-                'Are you sure you are handling your task object correctly for showing',
-                "as a string?\n"
-                ]
-            end
-          puts 'Your task list:'
-          puts my_list.show
+   class List
+     attr_reader :all_tasks
+
+     def initialize
+       @all_tasks = []
+     end
+
+     def add(task)
+       all_tasks << task
+     end
+
+     def show
+       all_tasks
+     end
+   end
+
+   class Task
+     attr_reader :description
+
+     def initialize(description)
+       @description = description
+     end
+
+   end
+
+
+# Actions
+
+   if __FILE__ == $PROGRAM_NAME
+     include Menu
+     include Promptable
+     ml = List.new
+     puts 'Please choose from the following list'
+     until ['q'].include?(user_input = prompt(show).downcase)
+       case user_input
+       when '1'
+         ml.add(Task.new(prompt('What is the task you would like to accomplish?')))
+       when '2'
+         puts ml.show
+       else
+         puts 'Try again, I did not understand.'
        end
+     end
+     puts 'Outro - Thanks for using our awesome menuing system!'
+   end
